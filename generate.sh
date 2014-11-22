2b899048b439bb4f2eebbaabf45ce1d00aa117b3
Chris Duncombe Rae <christopher.duncombe.rae@gmail.com>
2014-11-22 14:40:42 -0500

    Update website Sat Nov 22 14:40:42 EST 2014

---
+++ generate.sh
@@ -0,0 +1,36 @@
+# after you have made your edits run this script
+
+# do the stats on README.md
+
+./stats.sh
+
+files=$(git ls-files -m)
+num=$(git ls-files -m | wc -l)
+
+git add -A -v
+
+if [ ${#files} -le 50 ]; then
+	# git commit -m "Updating ${files}"
+	git commit -m "`echo Updating ${files}`"
+else
+	git commit -m "Updating ${num} files"
+fi
+
+# check if hugo is running
+if `ps xu | grep -v grep | grep hugo > /dev/null` ; then 
+	echo Hugo is running as a server. Kill the Hugo server then run hugo.
+	exit 1
+fi
+
+# Our chosen website generation tool Hugo (genuflect) does not recognise
+# symlinks (at least it follows them sometimes) and so hugo server --watch
+# does not update when README.md is changed. But we can't have the symlink
+# going the other way, because that complicates working with the repository
+# on GitHub (genuflect).  So touch a file we don't care about, tricking
+# stupid Hugo.
+
+touch content/.gitkeep
+hugo 
+
+
+
--- update.sh
+++ update.sh
@@ -1,27 +1,11 @@
-# after you have made your edits run this script
 
-# do the stats on README.md
-
-./stats.sh
-
-files=$(git ls-files -m)
-num=$(git ls-files -m | wc -l)
+# this script pushes the regenerated website to github
 
 git add -A -v
+git commit -m "Update website `date`"
 
-if [ ${#files} -le 50 ]; then
-	# git commit -m "Updating ${files}"
-	git commit -m "`echo Updating ${files}`"
-else
-	git commit -m "Updating ${num} files"
-fi
+git push
+# git subtree pull --prefix=public
+git subtree push --prefix=public origin gh-pages
 
-# our chosen website generation tool Hugo (genuflect) does not recognise
-# symlinks (at least it follows them sometimes) and so hugo server --watch
-# does not update when README.md is changed. But we can't have the symlink
-# going the other way, because that complicates working with the repository
-# on GitHub (genuflect).  So touch a file we don't care about, tricking
-# stupid Hugo.
-
-touch content/.gitkeep
 
