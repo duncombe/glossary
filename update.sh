4e3a84733f4e15229cc429db7cfd0aacdd91229c
Christopher Duncombe Rae <christopher.duncombe.rae@noaa.gov>
2014-12-01 16:50:04 +0000

    Updating update.sh

--- update.sh
+++ update.sh
@@ -3,9 +3,16 @@
 
 git add -A -v
 git commit -m "Update website `date`"
-
 git push
+
+# up to this part shopuld work ... 
+# BUT THE NEXT PART DOESN'T ...
 # git subtree pull --prefix=public
-git subtree push --prefix=public origin gh-pages
+# git subtree push --prefix=public origin gh-pages
+# It flakes out periodically for dumb stupid unknown reason, probably due
+# to user error (what else? dumb user.) Fail safe method?
+
+
+
 
 
