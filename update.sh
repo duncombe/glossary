2eac56c4dec212937f93f26c61ef5e74eee79bde
Christopher Duncombe Rae <christopher.duncombe.rae@noaa.gov>
2014-12-01 18:16:36 +0000

    Updating 7 files

--- update.sh
+++ update.sh
@@ -5,13 +5,25 @@ git add -A -v
 git commit -m "Update website `date`"
 git push
 
-# up to this part shopuld work ... 
+# up to this part should work ... 
 # BUT THE NEXT PART DOESN'T ...
 # git subtree pull --prefix=public
 # git subtree push --prefix=public origin gh-pages
+# 
 # It flakes out periodically for dumb stupid unknown reason, probably due
-# to user error (what else? dumb user.) Fail safe method?
+# to user error (what else? it's always the dumb user). Fail safe method?
 
+TEMPDIR=`mktemp -d`
+git clone -b gh-pages ssh://git@github.com/duncombe/glossary.git $TEMPDIR
+rm -rf $TEMPDIR/*
+cp -a public/* $TEMPDIR
+
+(
+    cd $TEMPDIR
+    git add -v -A
+    git commit -m "Updating website `date`"
+    git push 
+)
 
 
 
